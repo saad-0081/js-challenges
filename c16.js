378bd0c6abc9b442f2a1242c6e736c2dcc178359
saad-0081 <saadelmoudnib2006@gmail.com>
2026-09-18 12:13:00 +0200

    Update c16.js

---
+++ c16.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+let nomber1 = parseInt(prompt("Enter premiere nombre:"));
+let nomber2 = parseInt(prompt("Enter deuxieme nombre:"));
+let nomber3 = parseInt(prompt("Enter troisieme nombre:"));
+let moyenne = nomber1+nomber2+nombre3/3
+if(moyenne>=16){
+  console.log(` ${moyenne} La mention très bien .`);  
+}
+else if(moyenne>=14 && moyenne<16){
+   console.log(` ${moyenne} La mention bien .`);  
+}
+else if(moyenne>=12 && moyenne<14){
+   console.log(` ${moyenne}La mention assez bien .`);  
+}
+else if(moyenne>=10 && moyenne<12){
+   console.log(` ${moyenne} La mention passable .`);  
+}
+else {
+   console.log(` ${moyenne} La mention recale .`);  
+}
