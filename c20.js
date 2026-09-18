bf58b3a680daab05e89b1744c6fdb03191830862
saad-0081 <saadelmoudnib2006@gmail.com>
2026-09-18 23:52:21 +0300

    Add multiplication table generator in c20.js

---
+++ c20.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let nombre = parseInt(prompt("Entrez un nombre pour voir sa table :"));
+
+console.log("Table de multiplication de " + nombre + " (inversée) :");
+
+for (let i = 10; i >= 1; i--) {
+    let resultat = nombre * i;
+    console.log(`${nombre} x ${i} = ${resultat}`);
+}
