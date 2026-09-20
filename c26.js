e5902a24f08fa18942e35a238eaf7942f4903588
saad-0081 <saadelmoudnib2006@gmail.com>
2026-09-20 13:00:22 +0300

    Refactor multiplication logic and variable names

--- c26.js
+++ c26.js
@@ -1,16 +1,20 @@
 const prompt = require('prompt-sync')();
 
 let n = prompt("Combien d'éléments ?");
-let tableau = [];
+let tableau1 = [];
 
 for (let i = 0; i < n; i++) {
-    tableau.push(Number(prompt("Entrez le nombre " + (i + 1) + " :")));
+    let texte = prompt("Entrez le nombre " + (i + 1) + " :");
+    tableau1.push(Number(texte));
 }
 
-let facteur = Number(prompt("Entrez le facteur de multiplication :"));
-let resultat = [];
-for (let i = 0; i < tableau.length; i++) {
-    resultat.push(tableau[i] * facteur);
+let facteurTexte = prompt("Entrez le facteur de multiplication :");
+let facteur = Number(facteurTexte);
+
+let tableau2 = []; 
+for (let i = 0; i < tableau1.length; i++) {
+    let calcul = tableau1[i] * facteur;
+    tableau2.push(calcul);
 }
 
-console.log( "Tableau de multiplication" + resultat);
+console.log("Tableau multiplié : " + tableau2);
