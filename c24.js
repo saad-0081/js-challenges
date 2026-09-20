cf2d1f2a463b3ebbc752a31865e8822716285f75
saad-0081 <saadelmoudnib2006@gmail.com>
2026-09-20 12:56:25 +0300

    Refactor to use array for input and find max

--- c24.js
+++ c24.js
@@ -1,13 +1,17 @@
 const prompt = require('prompt-sync')();
 let n = prompt("Combien d'éléments ?");
-let max = -Infinity; 
+let tableau = [];
 
 for (let i = 0; i < n; i++) {
+    let texte = prompt("Entrez le nombre " + (i + 1) + " :");
+    tableau.push(Number(texte));
+}
 
-    let nombre = Number(prompt("Entrez le nombre " + (i + 1) + " :"));
-    if (nombre > max) {
+let max = tableau[0];
 
-        max = nombre;
+for (let i = 1; i < tableau.length; i++) {
+    if (tableau[i] > max) {
+        max = tableau[i];
     }
 }
 
